6acece8708c37aa88eb070c49cf3a1f9908a9c5c
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-30 18:55:20 -0500

    Rethink how we're going to handle deps since we can't handle multiple file uploads in the curl post with the same 'name'. Thinking about just referencing the sass files URI and pulling that down, parsing vars & recompiling...

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra/base'
 require 'mustache/sinatra'
 require 'sass'
+require 'json'
+require 'net/http'
+require 'uri'
 
 class App < Sinatra::Base
   register Mustache::Sinatra
@@ -47,34 +50,63 @@ class App < Sinatra::Base
   post '/api/?' do
     @title = "Sass Passed - here is your css!"
 
-    @domain = params[:domain]
-    @sass = params[:sass]
-    @sass_file = params['sass'][:filename]
-    @vars = params[:vars]
-    @compass = params[:compass]
+    if params[:domain]
+      @domain = params[:domain]
+    end
+
+    if params[:sass]
+      @sass = params[:sass]
+    end
+
+    if params['sass'][:filename]
+      @sass_file = params['sass'][:filename]
+    end
+
+    if params[:deps]
+      @dependancies = params[:deps]
+    end
+
+    if params[:vars]
+      @vars = params[:vars]
+    end
+
+    if params[:compass]
+      @compass = params[:compass]
+    end
 
     # Figure out which kind of sass file
-    if File.extname(@sass_file) == '.sass'
+    if @sass_file and File.extname(@sass_file) == '.sass'
       @type = 'sass/'
     else
       @type = 'scss/'
     end
 
-    File.open('uploads/' + @type + @sass_file, "w") do |f|
-      f.write(params['sass'][:tempfile].read)
+    if @type
+      File.open('uploads/' + @type + @sass_file, "w") do |f|
+        f.write(params['sass'][:tempfile].read)
+      end
     end
 
-    if @type == 'sass/'
-      sass :example
-    elsif @type == 'scss/'
-      scss :example
-    else
-      "File extension error"
-    end
+
+    {"params" => params}.to_json
+
+#    if @type == 'sass/'
+#      sass :example
+#    elsif @type == 'scss/'
+#      scss :example
+#    else
+#      "File extension error"
+#    end
   end
   
-  get '/test' do
-    scss :example
+  post '/test/?' do
+    @url = params[:url]
+    url = URI.parse(@url)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    "#{res.body}"
   end
   
 end
