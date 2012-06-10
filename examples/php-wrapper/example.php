ef26eebc35f37e2dba1e87e4c80c802a49bcb2b8
Brian Fegter <brian@fegter.com>
2012-06-10 00:55:40 -0400

    Removed unused PHP wrapper implementation file

--- examples/php-wrapper/example.php
+++ examples/php-wrapper/example.php
@@ -1,24 +1,40 @@
 <?php
-
-# DISCLAIMER - This is still very unstable. Please consider this an alpha implementation.
-
+# Get SASSy Goodness
 require_once 'PassTheSASS.class.php';
 
+# Obligatory Comment
+define('PASS_THE_SASS_DIR', dirname(__FILE__));
+
+/*	Debug Mode
+ *	Posts to http://pass-the-sass.herokuapp.com/test/
+ *	
+ *	Returns posted params
+ */
+define('PASS_THE_SASS_DEBUG', true);
+
+/*	Graceful Error Mode
+ *	Allows use of $sassy->get_errors() method.
+ *	
+ *	Falls back to PHP die() function for each error. 
+ */
+define('PASS_THE_SASS_GRACEFUL_ERRORS', true);
+
+
+# Object Arguments - see PassTheSASS::__construct() for argument documentation
 $args = array(
-
-	# Required
-	'sass_path' => '/full/path/to/your/sass/file',
-	'write_path' => '/full/path/to/your/output/dir/',
-	'vars' => array('var_name' => 'value', 'foo' => 'bar'), // Must be an associative array
-	'app' => 'application-name',
-	'uid' => 'www.yourdomain.com'
-
-	# Not Required
-	#'deps_dir' => '/full/path/to/dependencies/dir/' // Defaults to SASS path directory
-	#'deps' => array('file-name.scss', 'file-name.sass') // Must be scss or sass file types
-	#'api' => 'http://yourdomain.com/api' // Defaults to http://passthesass.com/api
-
+	'sass_path' => PASS_THE_SASS_DIR.'/sass/example.sass',
+	'write_path' => PASS_THE_SASS_DIR.'/css/style.css',
+	'vars' => array('primary_color' => '#000', 'secondary_color' => '#e3e3e3', 'var3' => '20px'),
+	'deps' => array('dependency1.scss', 'dependency2.scss', 'nested-dep.scss'),
+	'app' => rand(0, 9999)
 );
 
-$sassy = new PassTheSASS();
-$sassy->compile();
+# Instantiate A SASSy Object
+$sassy = new PassTheSASS($args);
+
+# Make SASSy Magic
+$result = $sassy->compile();
+
+# Debug that SASSy Mess
+$server_response = $sassy->response; // Response from the server
+$errors = $sassy->get_errors(); // Works with PASS_THE_SASS_GRACEFUL_MODE
